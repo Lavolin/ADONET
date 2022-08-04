db8c94cde7dca491859554a5e88f830eac3b8ef1
Lavolin <lavolin@gmail.com>
2022-08-04 08:34:21 +0300

    Add project files.

---
+++ ADONET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADONET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
